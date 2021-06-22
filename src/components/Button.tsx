1ba328a4723a2193df218e146c835c17065d26cb
mariaclarabs <clarabsouza@hotmail.com>
2021-06-21 21:49:16 -0300

    Create app/firebase config

---
+++ src/components/Button.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react"
+
+type ButtonProps = {
+  children?: string;
+}
+
+export function Button(props: ButtonProps) {
+  const [counter, setCounter] = useState(0);
+
+  function increment() {
+    setCounter(counter + 1);
+  }
+
+  return (
+    <button onClick={increment}>{props.children || 'Default' } ({counter})</button>
+  )
+}
